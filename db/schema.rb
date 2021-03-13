b14434128f4dc8c4a456028039cca088c5206720
Karl Heitmann <khf1988@gmail.com>
2021-03-13 17:43:03 -0300

    pet history agregado

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_204030) do
+ActiveRecord::Schema.define(version: 2021_03_13_204214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pet_histories", force: :cascade do |t|
+    t.float "weight"
+    t.string "height"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
