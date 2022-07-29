cc2b6182cb690dd4f74717100a4e36332b3f9457
Ludwig Valdenegro <ludwigval@gmail.com>
2022-07-29 16:53:21 -0400

    Crea relacion entre Modelos Pet y Pet_histories y su FK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_204214) do
+ActiveRecord::Schema.define(version: 2022_07_28_212625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_03_13_204214) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "pet_id", null: false
+    t.index ["pet_id"], name: "index_pet_histories_on_pet_id"
   end
 
   create_table "pets", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_03_13_204214) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "pet_histories", "pets"
 end
